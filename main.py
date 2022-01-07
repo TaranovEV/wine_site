b0a1cb26ecfac27eaf9765d43c1a0e239bc47c2b
taranovev <tar_ev@mail.ru>
2022-01-07 18:32:01 +0300

    Edit main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import datetime
 from collections import OrderedDict, defaultdict
 from http.server import HTTPServer, SimpleHTTPRequestHandler
@@ -5,33 +6,43 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 import pandas as pd
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-ALL_DEMAND = pd.read_excel('wine3.xlsx').fillna('').to_dict('record')
+parser = argparse.ArgumentParser(
+    description='Запускает сайт по адресу http://127.0.0.1:8000'
+)
+parser.add_argument('-p', '--path',
+                    help='Путь к файлу с напитками, формат .xlsx',
+                    type=str,
+                    default='wine3.xlsx')
+args = parser.parse_args()
+drinks = pd.read_excel(args.path).fillna('').to_dict('records')
 
 age = (
-    datetime.datetime.today() -
-    datetime.datetime.strptime('1920-01-01', '%Y-%m-%d')
-)
-age = int(age.days // (365.25))
-
-dict_demand = defaultdict(list)
-for demand in ALL_DEMAND:
-    dict_demand[demand['Категория']].append(demand)
-dict_demand = OrderedDict(sorted(dict_demand.items(), key=lambda x: x[0]))
-
-
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
+    datetime.datetime.today().year - 1920
 )
 
-template = env.get_template('template.html')
-rendered_page = template.render(
-    age=age,
-    dict_demand=dict_demand
-)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+def main():
+    drinks_for_template = defaultdict(list)
+    for drink in drinks:
+        drinks_for_template[drink['Категория']].append(drink)
+    drinks_for_template = OrderedDict(sorted(drinks_for_template.items(),
+                                      key=lambda x: x[0]))
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('template.html')
+    rendered_page = template.render(
+        age=age,
+        drinks_for_template=drinks_for_template
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+if __name__ in '__main__':
+    main()
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
